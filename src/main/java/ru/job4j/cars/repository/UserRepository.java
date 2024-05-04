ba59b3fe5398e265640201a3184698d9ca7bc839
apollobing <taras.career@gmail.com>
2024-05-04 16:38:10 +0200

    refactor: simplify UserRepository - 6. Лямбды и шаблон Command [#49295]

--- src/main/java/ru/job4j/cars/repository/UserRepository.java
+++ src/main/java/ru/job4j/cars/repository/UserRepository.java
@@ -1,18 +1,15 @@
 package ru.job4j.cars.repository;
 
 import lombok.AllArgsConstructor;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.query.Query;
 import ru.job4j.cars.model.User;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @AllArgsConstructor
 public class UserRepository {
-    private final SessionFactory sf;
+    private final CrudRepository crudRepository;
 
     /**
      * Save in DB.
@@ -20,16 +17,7 @@ public class UserRepository {
      * @return user with id.
      */
     public User create(User user) {
-        Session session = sf.openSession();
-        try {
-            session.beginTransaction();
-            session.persist(user);
-            session.getTransaction().commit();
-        } catch (Exception e) {
-            session.getTransaction().rollback();
-        } finally {
-            session.close();
-        }
+        crudRepository.run(session -> session.persist(user));
         return user;
     }
 
@@ -38,22 +26,7 @@ public class UserRepository {
      * @param user user.
      */
     public void update(User user) {
-        Session session = sf.openSession();
-        try {
-            session.beginTransaction();
-            session.createMutationQuery(
-                            "UPDATE User SET login = :fLogin,"
-                                    + "password = :fPassword WHERE id = :fId")
-                    .setParameter("fLogin", user.getLogin())
-                    .setParameter("fPassword", user.getPassword())
-                    .setParameter("fId", user.getId())
-                    .executeUpdate();
-            session.getTransaction().commit();
-        } catch (Exception e) {
-            session.getTransaction().rollback();
-        } finally {
-            session.close();
-        }
+        crudRepository.run(session -> session.merge(user));
     }
 
     /**
@@ -61,18 +34,10 @@ public class UserRepository {
      * @param userId ID.
      */
     public void delete(int userId) {
-        Session session = sf.openSession();
-        try {
-            session.beginTransaction();
-            User user = new User();
-            user.setId(userId);
-            session.remove(user);
-            session.getTransaction().commit();
-        } catch (Exception e) {
-            session.getTransaction().rollback();
-        } finally {
-            session.close();
-        }
+        crudRepository.run(
+                "delete from User where id = :fId",
+                Map.of("fId", userId)
+        );
     }
 
     /**
@@ -80,18 +45,7 @@ public class UserRepository {
      * @return list of users.
      */
     public List<User> findAllOrderById() {
-        Session session = sf.openSession();
-        List<User> users = new ArrayList<>();
-        try {
-            session.beginTransaction();
-            users = session.createQuery("FROM User ORDER BY id", User.class).list();
-            session.getTransaction().commit();
-        } catch (Exception e) {
-            session.getTransaction().rollback();
-        } finally {
-            session.close();
-        }
-        return users;
+        return crudRepository.query("from User order by id asc", User.class);
     }
 
     /**
@@ -99,18 +53,10 @@ public class UserRepository {
      * @return Optional or user.
      */
     public Optional<User> findById(int userId) {
-        Session session = sf.openSession();
-        Optional<User> user = Optional.empty();
-        try {
-            session.beginTransaction();
-            user = Optional.of(session.get(User.class, userId));
-            session.getTransaction().commit();
-        } catch (Exception e) {
-            session.getTransaction().rollback();
-        } finally {
-            session.close();
-        }
-        return user;
+        return crudRepository.optional(
+                "from User where id = :fId", User.class,
+                Map.of("fId", userId)
+        );
     }
 
     /**
@@ -119,21 +65,10 @@ public class UserRepository {
      * @return list of users.
      */
     public List<User> findByLikeLogin(String key) {
-        Session session = sf.openSession();
-        List<User> users = new ArrayList<>();
-        try {
-            session.beginTransaction();
-            Query<User> query =
-                    session.createQuery("FROM User WHERE login LIKE :fKey", User.class);
-            query.setParameter("fKey", "%" + key + "%");
-            users = query.list();
-            session.getTransaction().commit();
-        } catch (Exception e) {
-            session.getTransaction().rollback();
-        } finally {
-            session.close();
-        }
-        return users;
+        return crudRepository.query(
+                "from User where login like :fKey", User.class,
+                Map.of("fKey", "%" + key + "%")
+        );
     }
 
     /**
@@ -142,19 +77,9 @@ public class UserRepository {
      * @return Optional or user.
      */
     public Optional<User> findByLogin(String login) {
-        Session session = sf.openSession();
-        Optional<User> user = Optional.empty();
-        try {
-            session.beginTransaction();
-            Query<User> query = session.createQuery("FROM User WHERE login = :fLogin", User.class);
-            query.setParameter("fLogin", login);
-            user = query.uniqueResultOptional();
-            session.getTransaction().commit();
-        } catch (Exception e) {
-            session.getTransaction().rollback();
-        } finally {
-            session.close();
-        }
-        return user;
+        return crudRepository.optional(
+                "from User where login = :fLogin", User.class,
+                Map.of("fLogin", login)
+        );
     }
 }
