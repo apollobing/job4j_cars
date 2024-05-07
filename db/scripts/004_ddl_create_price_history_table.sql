2bc39ce3610f38333b2c5cf9be4c7155c281b51a
apollobing <taras.career@gmail.com>
2024-05-07 18:56:56 +0200

    refactor: rename column name - 1. ToMany [#301848]

--- db/scripts/004_ddl_create_price_history_table.sql
+++ db/scripts/004_ddl_create_price_history_table.sql
@@ -1,8 +1,8 @@
 create table price_history
 (
-   id       serial primary key,
-   before   bigint not null,
-   after    bigint not null,
-   created  timestamp without time zone default now(),
-   post_id  int REFERENCES auto_post(id)
+   id               serial primary key,
+   before           bigint not null,
+   after            bigint not null,
+   created          timestamp without time zone default now(),
+   auto_post_id     int REFERENCES auto_post(id)
 );
