a000717658ebd589bf553ac1f9dc87de9c3b73eb
apollobing <taras.career@gmail.com>
2024-05-22 22:37:57 +0200

    feat: add methods to PostRepository - 1. Фильтры для объявлений машин [#4745]

--- src/main/java/ru/job4j/cars/repository/PostRepository.java
+++ src/main/java/ru/job4j/cars/repository/PostRepository.java
@@ -4,7 +4,9 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Repository;
 import ru.job4j.cars.model.Post;
 
-import java.util.List;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Collection;
 import java.util.Map;
 import java.util.Optional;
 
@@ -23,28 +25,55 @@ public class PostRepository {
         crudRepository.run(session -> session.merge(post));
     }
 
-    public void delete(int postId) {
-        crudRepository.run(
-                "delete from Post where id = :fId",
-                Map.of("fId", postId)
-        );
+    public void delete(Post post) {
+        crudRepository.run(session -> session.remove(post));
     }
 
-    public List<Post> findAllOrderById() {
-        return crudRepository.query("from Post order by id asc", Post.class);
+    public Collection<Post> findAllOrderById() {
+        return crudRepository.query(
+                "FROM Post p"
+                        + " JOIN FETCH p.user JOIN FETCH p.price JOIN FETCH p.participates JOIN FETCH p.car c"
+                        + " JOIN FETCH c.engine JOIN FETCH c.owner JOIN FETCH c.owner.user JOIN FETCH c.owners o"
+                        + " JOIN FETCH o.user ORDER BY p.id DESC", Post.class);
     }
 
     public Optional<Post> findById(int postId) {
         return crudRepository.optional(
-                "from Post where id = :fId", Post.class,
+                "FROM Post p"
+                        + " JOIN FETCH p.user JOIN FETCH p.price JOIN FETCH p.participates JOIN FETCH p.car c"
+                        + " JOIN FETCH c.engine JOIN FETCH c.owner JOIN FETCH c.owner.user JOIN FETCH c.owners o"
+                        + " JOIN FETCH o.user WHERE p.id = :fId", Post.class,
                 Map.of("fId", postId)
         );
     }
 
-    public List<Post> findByName(String postName) {
+    public Collection<Post> findByName(String postName) {
         return crudRepository.query(
-                "from Post where name = :fPostName", Post.class,
+                "FROM Post p"
+                        + " JOIN FETCH p.user JOIN FETCH p.price JOIN FETCH p.participates JOIN FETCH p.car c"
+                        + " JOIN FETCH c.engine JOIN FETCH c.owner JOIN FETCH c.owner.user JOIN FETCH c.owners o"
+                        + " JOIN FETCH o.user WHERE p.name = :fPostName ORDER BY p.id DESC", Post.class,
                 Map.of("fPostName", postName)
+        );
+    }
+
+    public Collection<Post> findNew() {
+        LocalDateTime today = LocalDate.now().atStartOfDay();
+        return crudRepository.query(
+                "FROM Post p"
+                        + " JOIN FETCH p.user JOIN FETCH p.price JOIN FETCH p.participates JOIN FETCH p.car c"
+                        + " JOIN FETCH c.engine JOIN FETCH c.owner JOIN FETCH c.owner.user JOIN FETCH c.owners o"
+                        + " JOIN FETCH o.user WHERE p.created >= :fToday ORDER BY p.id DESC", Post.class,
+                Map.of("fToday", today)
+        );
+    }
+
+    public Collection<Post> findWithPhoto() {
+        return crudRepository.query(
+                "FROM Post p"
+                        + " JOIN FETCH p.user JOIN FETCH p.price JOIN FETCH p.participates JOIN FETCH p.car c"
+                        + " JOIN FETCH c.engine JOIN FETCH c.owner JOIN FETCH c.owner.user JOIN FETCH c.owners o"
+                        + " JOIN FETCH o.user WHERE p.fileId > 0 ORDER BY p.id DESC", Post.class
         );
     }
 }
