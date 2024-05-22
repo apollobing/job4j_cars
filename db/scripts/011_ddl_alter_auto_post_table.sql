8881969582754d9e3d8686e2857ec94e8eaa04bb
apollobing <taras.career@gmail.com>
2024-05-23 00:01:06 +0200

    fix: split db script - 1. Фильтры для объявлений машин [#4745]

--- db/scripts/011_ddl_alter_auto_post_table.sql
+++ db/scripts/011_ddl_alter_auto_post_table.sql
@@ -1,4 +1,2 @@
 ALTER TABLE auto_post
-ADD COLUMN car_id int not null unique references car(id),
-ADD COLUMN name varchar not null,
-ADD COLUMN file_id int;
+ADD COLUMN car_id int not null unique references car(id);
---
+++ db/scripts/012_ddl_alter_auto_post_table.sql
@@ -0,0 +1,2 @@
+ALTER TABLE auto_post
+ADD COLUMN name varchar not null;
